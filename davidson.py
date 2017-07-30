5e33c092509bfc4b36832944d9592beb90e48139
Sreeganesh Balasubramani <sreeganb@volga.ps.uci.edu>
2017-07-29 21:32:22 -0700

    made some changes

--- davidson.py
+++ davidson.py
@@ -8,6 +8,7 @@
 import numpy as np
 import math
 import time 
+from numpy.linalg import multi_dot
 
 # Build a fake sparse symmetric matrix 
 n = 100
@@ -19,21 +20,30 @@ A = A + sparsity*np.random.randn(n,n)
 A = (A.T + A)/2
 
 tol = 1e-8              # Convergence tolerance
-mmax = n//2             # Maximum number of iterations
+mmax = n/2              # Maximum number of iterations
 
 # Setup the subspace trial vectors
 k = 8
-eig = 4
+eig = 3
 t = np.eye(n,k) # trial vectors
 v = np.zeros((n,n))
 I = np.eye(n)
-
 #start_davidson = time.time()
-
+#-------------------------------------------------------------------------------
+# Begin iterations  
+#-------------------------------------------------------------------------------
+iter = 0
 for m in xrange(k,mmax,k):
-    if m <= k:
-        for j in xrange(0,k)
-            v[:,j] = t[:,j]/np.linalg.norm(t[:,j]) # normalize the vectors
+    iter = iter + 1
+    print "Iteration no:" % iter
+    # Matrix-vector products
+    T = multidot([t.T,A,t])
+    
 
 
-
+#for m in xrange(k,mmax,k):
+#    if m <= k:
+#        for j in xrange(0,k):
+#            v[:,j] = t[:,j]/np.linalg.norm(t[:,j]) # normalize the vectors
+#print((t))
+#print ((v))
